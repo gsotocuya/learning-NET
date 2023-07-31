169874c08fd3917feb64a83c53cc9613d014c69f
grimaldo.soto <Grimaldo.SOTO@icloud.com>
2023-07-30 22:08:48 -0500

    CodigoFacilito DemoTaller

---
+++ DemoTallerCodigoFacilito/DemoTallerCodigoFacilito/Data/StoreDbContext.cs
@@ -0,0 +1,13 @@
+using DemoTallerCodigoFacilito.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoTallerCodigoFacilito.Data;
+
+public class StoreDbContext : DbContext
+{
+    public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+    {
+    }
+    public DbSet<ProductEntity> Products { get; set; }
+
+}

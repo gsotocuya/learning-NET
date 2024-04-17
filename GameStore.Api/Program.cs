8ee789d550b69b868ed5b5f3b36062b9bfada7a2
Grimaldo SOTO <grimaldo.soto@outlook.com>
2024-04-17 15:11:52 -0500

    04 Using Data Transfer Objects DTOs

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,9 +1,32 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+List<GameDto> games = [
+    new (
+        1,
+        "Street Figther II",
+        "Fighting",
+        19.99M,
+        new DateOnly(1992, 7, 15)),
+    new (
+        2,
+        "Final Fantasy XIV",
+        "Roleplaying",
+        59.99M,
+        new DateOnly(2000, 9,30)),
+    new (
+        3,
+        "FIFA 23",
+        "Sports",
+        69.99M,
+        new DateOnly(2022, 9, 27))
+    ];
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
